be10b3d43e77ec1954055c60d90050dde8c50eb3
DESKTOP-4UDHQB4\User <potionstory@gmail.com>
2021-05-18 17:54:07 +0900

    [frontend] SelectBox Component add

---
+++ frontend/src/components/SelectBox/SelectBox.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useCallback } from 'react';
+import map from 'lodash/map';
+import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { SelectBoxWrap } from './style';
+
+const SelectBox = ({ value, list, onSelectChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onOpenHandle = useCallback(() => {
+    setIsOpen(isOpen => !isOpen);
+  }, []);
+
+  const onHandleClick = useCallback((id) => {
+    setIsOpen(isOpen => !isOpen);
+    onSelectChange(id);
+  }, []);
+
+  const isScroll = list.length * 41 - 1 > 4 * 41 - 1
+
+  return (
+    <SelectBoxWrap isScroll={isScroll}>
+      <div className="selected">
+        <span className="item">{value}</span>
+        <button type="button" onClick={onOpenHandle}>
+          <motion.span
+            animate={{ rotate: isOpen ? 180 : 0 }}
+            transition={{
+              ease: 'backInOut',
+            }}
+          >
+            <FontAwesomeIcon icon={faAngleDown} />
+          </motion.span>
+        </button>
+      </div>
+      <motion.ul
+        animate={{ height: isOpen ? isScroll ? 4 * 41 - 1 : list.length * 41 - 1 : 0 }}
+        transition={{
+          ease: 'backInOut',
+        }}
+      >
+        {map(list, item => {
+          const { id, name } = item;
+
+          return (
+            <li key={id}>
+              <button type="button" onClick={() => onHandleClick(id)}>{name}</button>
+            </li>
+          );
+        })}
+      </motion.ul>
+    </SelectBoxWrap>
+  );
+};
+
+export default SelectBox;
---
+++ frontend/src/components/SelectBox/style.js
@@ -0,0 +1,87 @@
+import styled, { css } from 'styled-components';
+import { color } from '../../lib/styles/palette';
+import { ellipsis } from '../../lib/styles/util';
+
+export const SelectBoxWrap = styled.div`
+  position: relative;
+  width: 220px;
+  margin-left: 12px;
+  .selected {
+    display: flex;
+    overflow: hidden;
+    position: relative;
+    z-index: 10;
+    height: 40px;
+    border-radius: 4px;
+    background-color: ${({ theme }) => theme.sub};
+    .item {
+      flex: 1;
+      padding: 8px 20px;
+      font-size: 0.625rem;
+      color: ${({ theme }) => theme.title};
+      font-weight: 600;
+      line-height: 1.5rem;
+      ${ellipsis};
+    }
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 40px;
+      height: 40px;
+      background-color: ${({ theme }) => theme.theme};
+      span {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        svg {
+          font-size: 1.5rem;
+          color: ${({ theme }) => theme.title};
+          opacity: 0.6;
+          transition: all 0.2s ease-out;
+        }
+        &:hover {
+          svg {
+            opacity: 1;
+          }
+        }
+      }
+    }
+  }
+  ul {
+    ${({ isScroll }) => !isScroll ? 'overflow: hidden;' : 'overflow-y: auto;'};
+    position: absolute;
+    top: 52px;
+    left: 0;
+    z-index: 15;
+    width: 100%;
+    height: 0;
+    border-radius: 4px;
+    background-color: ${({ theme }) => theme.title};
+    li {
+      display: flex;
+      border-top: 1px dashed ${color.gray};
+      button {
+        flex: 1;
+        padding: 8px 20px;
+        box-sizing: border-box;
+        font-size: 0.625rem;
+        color: ${({ theme }) => theme.main};
+        font-weight: 600;
+        line-height: 1.5rem;
+        text-align: left;
+        opacity: 0.6;
+        ${ellipsis};
+        &:hover {
+          background-color: ${({ theme }) => theme.text};
+          opacity: 1;
+        }
+      }
+      &:first-child {
+        border-top: 0 none;
+      }
+    }
+  }
+`;
